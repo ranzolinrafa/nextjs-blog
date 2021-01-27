4699cdbf1e98c596b94586fc744ac60e95687205
rafaelpedroso <rafael.pedroso@compasso.com.br>
2021-01-27 01:57:14 -0300

    First Commit

---
+++ pages/index.js
@@ -0,0 +1,45 @@
+import Head from "next/head";
+import Link from "next/link";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import Date from "../components/Date";
+
+import { getSortedPostsData } from "../lib/posts";
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Olá, eu sou o Stone</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on)
+        </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+
+  return { props: { allPostsData } };
+}
